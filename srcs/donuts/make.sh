6a820eda285862ad2ca51c37f832fa732802e8f1
wbousfi <wael.bousfira@protonmail.com>
2022-10-29 03:06:28 +0100

    update23732

--- srcs/donuts/make.sh
+++ srcs/donuts/make.sh
@@ -3,7 +3,7 @@ BLUE="\033[1;34m"
 BACK_GREEN="\033[1;42m"
 BACK_PURPLE="\033[1;43m"
 END="\033[0m"
-echo "${BLUE}Do you want to test the functions printf by printing a 3D Donuts? Type ${END}${BACK_GREEN} Y ${END}${BLUE} for yes OR ${BACK_PURPLE} Press ENTER ${END}\n${END}"
+echo "${BLUE}Do you want to test the functions printf by printing a 3D Donuts? Type ${END}${BACK_GREEN} Y ${END}${BLUE} for yes OR ${END}${BLUE}${BACK_PURPLE} Press ENTER ${END}\n"
 read input
 if [ "$input" = "Y" ] || [ "$input" = "y" ]; then
 	cd srcs/donuts/
